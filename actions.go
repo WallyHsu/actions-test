656c604d5de6fc614c0b3fff20bff1dc10f33b49
wally <wally_hsu@mail.chungyo.net>
2023-12-27 14:36:56 +0800

    [修改]測試workflow

--- actions.go
+++ actions.go
@@ -14,6 +14,6 @@ func Demo() {
 	fmt.Printf("Go version: %s\n", runtime.Version())
 	fmt.Printf("GOOS: %s\n", runtime.GOOS)
 	fmt.Printf("GOARCH: %s\n", runtime.GOARCH)
-
+	// 測試
 	fmt.Println(quote.Go())
 }
